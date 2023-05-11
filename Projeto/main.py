630bfe11ce776da5b5d20678fd33df39d9f52d4b
MathGabr <50830710+MathGabr@users.noreply.github.com>
2023-05-10 21:38:21 -0300

    deletando

--- Projeto/main.py
+++
@@ -1,8 +0,0 @@
-from pack.bib import *
-
-while(True):
-    resp=int(input(f"\nSelecione o Exercicio que deseja executar (escreva exit para sair)\nR: "))
-
-    if resp == 1:
-        num=int(input("\nInsira um número: "))
-        exercicio1(num)     
--- Projeto/pack/bib.py
+++
@@ -1,7 +0,0 @@
-def exercicio1(num):
-    antecessor = num - 1
-    sucessor = num + 1
-    
-    print("\nAntecessor: ", antecessor)
-    print("Sucessor: ", sucessor)
-
